d2c2897c3a1c61545fe99a110b43cab417db5ade
Boris Mezhibovskiy <mez13526@gmail.com>
2016-07-09 23:12:23 -0700

    Fixed matrix multiplication bug.

--- Win32Project2/Matrix4x4.cpp
+++ Win32Project2/Matrix4x4.cpp
@@ -60,22 +60,22 @@ Matrix4x4 Matrix4x4::operator*(Matrix4x4 m2) {
 	return Matrix4x4(
 		matrix[0][0] * m2.matrix[0][0] + matrix[0][1] * m2.matrix[1][0] + matrix[0][2] * m2.matrix[2][0] + matrix[0][3] * m2.matrix[3][0],
 		matrix[0][0] * m2.matrix[0][1] + matrix[0][1] * m2.matrix[1][1] + matrix[0][2] * m2.matrix[2][1] + matrix[0][3] * m2.matrix[3][1],
-		matrix[0][0] * m2.matrix[0][2] + matrix[0][2] * m2.matrix[1][2] + matrix[0][2] * m2.matrix[2][2] + matrix[0][3] * m2.matrix[3][2],
-		matrix[0][0] * m2.matrix[0][3] + matrix[0][3] * m2.matrix[1][3] + matrix[0][2] * m2.matrix[2][3] + matrix[0][3] * m2.matrix[3][3],
+		matrix[0][0] * m2.matrix[0][2] + matrix[0][1] * m2.matrix[1][2] + matrix[0][2] * m2.matrix[2][2] + matrix[0][3] * m2.matrix[3][2],
+		matrix[0][0] * m2.matrix[0][3] + matrix[0][1] * m2.matrix[1][3] + matrix[0][2] * m2.matrix[2][3] + matrix[0][3] * m2.matrix[3][3],
 
 		matrix[1][0] * m2.matrix[0][0] + matrix[1][1] * m2.matrix[1][0] + matrix[1][2] * m2.matrix[2][0] + matrix[1][3] * m2.matrix[3][0],
 		matrix[1][0] * m2.matrix[0][1] + matrix[1][1] * m2.matrix[1][1] + matrix[1][2] * m2.matrix[2][1] + matrix[1][3] * m2.matrix[3][1],
-		matrix[1][0] * m2.matrix[0][2] + matrix[1][2] * m2.matrix[1][2] + matrix[1][2] * m2.matrix[2][2] + matrix[1][3] * m2.matrix[3][2],
-		matrix[1][0] * m2.matrix[0][3] + matrix[1][3] * m2.matrix[1][3] + matrix[1][2] * m2.matrix[2][3] + matrix[1][3] * m2.matrix[3][3],
+		matrix[1][0] * m2.matrix[0][2] + matrix[1][1] * m2.matrix[1][2] + matrix[1][2] * m2.matrix[2][2] + matrix[1][3] * m2.matrix[3][2],
+		matrix[1][0] * m2.matrix[0][3] + matrix[1][1] * m2.matrix[1][3] + matrix[1][2] * m2.matrix[2][3] + matrix[1][3] * m2.matrix[3][3],
 
 		matrix[2][0] * m2.matrix[0][0] + matrix[2][1] * m2.matrix[1][0] + matrix[2][2] * m2.matrix[2][0] + matrix[2][3] * m2.matrix[3][0],
 		matrix[2][0] * m2.matrix[0][1] + matrix[2][1] * m2.matrix[1][1] + matrix[2][2] * m2.matrix[2][1] + matrix[2][3] * m2.matrix[3][1],
-		matrix[2][0] * m2.matrix[0][2] + matrix[2][2] * m2.matrix[1][2] + matrix[2][2] * m2.matrix[2][2] + matrix[2][3] * m2.matrix[3][2],
-		matrix[2][0] * m2.matrix[0][3] + matrix[2][3] * m2.matrix[1][3] + matrix[2][2] * m2.matrix[2][3] + matrix[2][3] * m2.matrix[3][3],
+		matrix[2][0] * m2.matrix[0][2] + matrix[2][1] * m2.matrix[1][2] + matrix[2][2] * m2.matrix[2][2] + matrix[2][3] * m2.matrix[3][2],
+		matrix[2][0] * m2.matrix[0][3] + matrix[2][1] * m2.matrix[1][3] + matrix[2][2] * m2.matrix[2][3] + matrix[2][3] * m2.matrix[3][3],
 
 		matrix[3][0] * m2.matrix[0][0] + matrix[3][1] * m2.matrix[1][0] + matrix[3][2] * m2.matrix[2][0] + matrix[3][3] * m2.matrix[3][0],
 		matrix[3][0] * m2.matrix[0][1] + matrix[3][1] * m2.matrix[1][1] + matrix[3][2] * m2.matrix[2][1] + matrix[3][3] * m2.matrix[3][1],
-		matrix[3][0] * m2.matrix[0][2] + matrix[3][2] * m2.matrix[1][2] + matrix[3][2] * m2.matrix[2][2] + matrix[3][3] * m2.matrix[3][2],
-		matrix[3][0] * m2.matrix[0][3] + matrix[3][3] * m2.matrix[1][3] + matrix[3][2] * m2.matrix[2][3] + matrix[3][3] * m2.matrix[3][3]
+		matrix[3][0] * m2.matrix[0][2] + matrix[3][1] * m2.matrix[1][2] + matrix[3][2] * m2.matrix[2][2] + matrix[3][3] * m2.matrix[3][2],
+		matrix[3][0] * m2.matrix[0][3] + matrix[3][1] * m2.matrix[1][3] + matrix[3][2] * m2.matrix[2][3] + matrix[3][3] * m2.matrix[3][3]
 		);
 }
